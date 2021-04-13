76572e7318738ca340dad7e06c45017ac4a62bab
Dorian Mazur <d.mazur@digitalcolliers.com>
2021-04-13 12:31:05 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ClickToLazyLoad.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="load">Lazy Load {{ cutNameIfNeeded() }} </button>
+    <component v-if="show" :is="lazyLoad" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+export default defineComponent({
+  setup() {
+    const cutNameIfNeeded = () => {
+      const name = "TestName";
+      const shopName = "TestShopName";
+      if (shopName.length >= 25) {
+        return name.length >= 25 ? `${shopName.slice(0, 22)}...` : name;
+      }
+      return shopName;
+    };
+    return { cutNameIfNeeded };
+  },
+  data() {
+    return {
+      show: false,
+      lazyLoad: null,
+    };
+  },
+  methods: {
+    load() {
+      import("@/LazyLoad.vue").then((mod) => {
+        console.log(mod.default);
+        this.lazyLoad = mod.default;
+        this.show = true;
+      });
+    },
+  },
+});
+</script>
